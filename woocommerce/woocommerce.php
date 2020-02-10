ad8812be0e23214686ab0aa79f0470749f0ea79e
pedram katanchi <pedramkatanchi@webmapp.it>
2020-02-10 13:28:13 +0100

    changed email before order table

--- woocommerce/woocommerce.php
+++ woocommerce/woocommerce.php
@@ -1243,3 +1243,12 @@ function woocommerce_button_proceed_to_checkout() {
 	</a>
 	<?php
 }
+
+
+add_action ('woocommerce_email_after_order_table','custom_message_client_emails_onhold_partial');
+function custom_message_client_emails_onhold_partial($order, $sent_to_admin, $plain_text, $email) {
+	$order_status = $order->get_status();
+		?>
+		<h3><?php echo __('The balance is to be paid 30 days before departure', 'wm-child-verdenatura'); print_r($order_status)?></h3>
+		<?php
+}
