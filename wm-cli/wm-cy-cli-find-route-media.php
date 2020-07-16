10830f68d9bbc30a822cef9e45eb02fe136db1c3
pedram katanchi <macbook@MacBook-Pro.station>
2020-07-16 12:49:06 +0200

    updated em-cy-cli-file-route-media

--- wm-cli/wm-cy-cli-find-route-media.php
+++ wm-cli/wm-cy-cli-find-route-media.php
@@ -16,19 +16,39 @@ $wm_cy_find_route_media = function( $args, $assoc_args )
     foreach ($args as $route_id) {
         //get post language
         $post_lang = apply_filters( 'wpml_post_language_details', NULL, $route_id );
-        WP_CLI::line( 'Route language is: '.$post_lang.'');
+        WP_CLI::line( 'Route language is: '.$post_lang['language_code'].'');
         
         //get wpml default language
         $default_lang = apply_filters('wpml_default_language', NULL );
         WP_CLI::line( 'WPML default languages is: '.$default_lang.'');
-
-        //This route image
-        $this_post_thumb = get_post_thumbnail_id( $route_id );
-        WP_CLI::line( 'Route featured image ID: '.$this_post_thumb.'');
-
+        
         // Route default language Id
         $post_default_language_id = apply_filters( 'wpml_object_id', $route_id, 'route', FALSE, $default_lang );
         WP_CLI::line( 'Route default language ID: '.$post_default_language_id.'');
+
+        //This route image ID
+        $this_post_thumb_id = get_post_thumbnail_id( $route_id );
+        WP_CLI::line( 'Route featured image ID: '.$this_post_thumb_id.'');
+
+        //get post language
+        $image_lang = apply_filters( 'wpml_post_language_details', NULL, $this_post_thumb_id );
+        WP_CLI::line( 'Route featured image language is: '.$image_lang['language_code'].'');
+
+        // ID of the Featured image in the language of current route 
+        //$featured_image_id_current_lang = apply_filters( 'wpml_object_id', $this_post_thumb_id, 'attachment', FALSE, $post_lang['language_code'] );
+        //WP_CLI::line( 'ID of featured image in route current language: '.$featured_image_id_current_lang.'');
+
+        $calendar_json = site_url().'/wp-json/wp/v2/media/'.$this_post_thumb_id;
+        $featured_image_json = json_decode(file_get_contents($calendar_json),TRUE);
+        foreach ($featured_image_json['wpml_translations'] as $c) {
+            foreach ($c as $d) {
+                if ($d == 'it_IT') {
+                    $featured_image_id_current_lang = $c['id'];
+                }
+            }
+        }
+        WP_CLI::line( 'ID of featured image in route current language: '.$featured_image_id_current_lang.'');
+        WP_CLI::line( ' ');
 
     }    
 
