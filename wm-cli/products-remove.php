5694cd37cbd078842ffdc9550765d1eb5ac78f0a
Pedram Katanchi <pedramkat@gmail.com>
2021-07-21 16:15:18 +0200

    Added post per page argument as a parameter

--- wm-cli/products-remove.php
+++ wm-cli/products-remove.php
@@ -1,20 +1,25 @@
 <?php
 
-# Removes those products that were created from Onclick form 
+# Removes those products that were created from Onclick form, Accepts one numeric argument as the number of post_per_page 
 #
 # $ wp foo bar --append=qux
 # Success: bar qux
 
 /**
- * Removes those products that were created from Onclick form 
+ * Removes those products that were created from Onclick form, Accepts one numeric argument as the number of post_per_page 
  *
  *
  * @when after_wp_load
  */
 $wm_product_remove = function( $args, $assoc_args )
 {
+    if (isset($args[0]) && is_numeric($args[0]) && $args[0] > 0) {
+        $post_per_page = $args[0];
+    } else {
+        $post_per_page = 200;
+    }
     $the_query = array(
-        'posts_per_page'   => 200,
+        'posts_per_page'   => $post_per_page,
         'post_type'        => 'product',
         'fields'           => 'ids',
         'date_query' => array(
